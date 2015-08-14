cbcc0ee0370e48b50ca0cd83156bd0efe38b2ac7
root <root@debian.oplink.net>
2015-08-13 21:05:41 -0500

    qowiuhfoqiwfjhoqiw

---
+++ run-droopescan.sh
@@ -0,0 +1,12 @@
+set -e 
+set -u
+
+cd droopescan
+cwd=/root/1kk_scan/split/
+for file in ${cwd}part*; do
+	results_dir=${cwd}results-$(basename $file)/
+	echo $results_dir
+	url_file=$file
+	mkdir -p $results_dir;
+	./droopescan scan -U $url_file --error-log ${results_dir}error-log.txt --massscan-override -e v 1> ${results_dir}results.json 2>${results_dir}stderr.txt &
+done
