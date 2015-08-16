b3c0e911b53809b812f1c6c53fc63b5c450446d3
root <root@debian.oplink.net>
2015-08-16 16:41:14 -0500

    bleh

--- dns_80.py
+++ dns_80.py
@@ -1,29 +1,36 @@
+from __future__ import print_function
 import sys
+from collections import deque
 
-dns_file = 'dns_records/20150606_dnsrecords_all'
-ip_files = ['port_scan/port_scan_1.txt', 'port_scan/port_scan_2.txt']
+argv = sys.argv
 
-out_file = 'dns_open_80.txt'
+dns_file = argv[1]
+file = argv[2]
+out_file = argv[3]
+
 out_fh = open(out_file, 'w')
 
-for file in ip_files:
-	ip_dict = {}
-	with open(file) as fh:
-		for line in fh:
-			ip_dict[line.rstrip()] = True
+ip_dict = {}
+latest_domains = deque(maxlen=10000)
+with open(file) as fh:
+	for line in fh:
+		ip_dict[line.rstrip()] = True
 
-		with open(dns_file) as dns_fh:
-			i = 0 
-			for line in dns_fh:
-				s = line.split(",")
-				domain, record, val = s[0], s[1], s[2]
-				if record == "a":
-					if val.strip() in ip_dict:
-						out_fh.write(line)
-				
-				if i % 1000 == 0 and i != 0:
-					print(".",)
+	with open(dns_file) as dns_fh:
+		i = 0 
+		for line in dns_fh:
+			s = line.split(",")
+			domain, record, val = s[0], s[1], s[2]
+			already_added = domain in latest_domains
+			if record == "a" and not already_added:
+				if val.strip() in ip_dict:
+					out_string = val.strip() + " " + domain.strip() + "\n"
+					out_fh.write(out_string)
+				latest_domains.append(domain)
+			
+			if i % 1000 == 0 and i != 0:
+				print(".", end="")
 
-				i += 1
+			i += 1
 						
 out_fh.close()
